091d4865303adb1da5971a1ec4ca00931817a9c2
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-04-16 17:44:22 +0200

    wip

--- frontend/components/Books.js
+++ frontend/components/Books.js
@@ -16,46 +16,16 @@ export default function Books() {
   }, [])
 
   const fetchBooks = () => {
-    fetch('/api/books')
-      .then(res => {
-        if (!res.ok) {
-          throw new Error('Network response was not ok')
-        }
-        const contentType = res.headers.get('Content-Type')
-        if (contentType.includes('application/json')) {
-          return res.json()
-        }
-      })
-      .then(data => {
-        setBooks(data)
-      })
-      .catch(err => console.error('Failed to fetch books', err))
+
   }
 
   const deleteBook = id => {
-    setBookForm(initialForm)
-    fetch(`/api/books/${id}`, { method: 'DELETE' })
-      .then(() => fetchBooks())
-      .catch(err => console.error('Failed to delete the book', err))
+
   }
 
   const onSubmit = (event) => {
     event.preventDefault()
-    const url = bookForm.id ? `/api/books/${bookForm.id}` : '/api/books'
 
-    fetch(url, {
-      method: bookForm.id ? 'PUT' : 'POST',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        'X-Bloom': 'full-stack-web',
-      }),
-      body: JSON.stringify(bookForm),
-    })
-      .then(() => {
-        fetchBooks()
-        setBookForm(initialForm)
-      })
-      .catch(err => console.error('Failed to save the book', err))
   }
 
   const onChange = (event) => {
